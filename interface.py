6bf1b13db2c1507ce50dcc891c82d19a04800eca
Flora-Chai <151617124+Flora-Chai@users.noreply.github.com>
2025-02-26 13:10:47 +0800

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,93 @@
+"""
+This project is based on https://github.com/omkarkshet/Diabetes-Prediction-Using-Machine-Learning-Techniques. We modified the code to fit our needs.
+We modified the parameters to fit our needs and added a few more parameters to the code.
+We added the detailed explanation of the code.
+If you have any question when running the app, please follow the instructions below.
+https://github.com/omkarkshet/Diabetes-Prediction-Using-Machine-Learning-Techniques
+"""
+
+import numpy as np
+import pickle
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+
+# Define a flask app
+app = Flask(__name__)
+
+model=pickle.load(open('model.pkl','rb'))
+
+print('Model loaded. Start serving...')
+
+print('Check http://127.0.0.1:5000/')
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # This function is called when a user visits the website.
+    # It returns the HTML content of the main page.
+    # The main page is defined in the file templates/index.html.
+    # The @app.route() decorator is a way to associate this function
+    # with a particular URL. In this case, when the user visits
+    # http://localhost:5000/, this function is called.
+    # The "GET" argument specifies that this function should be
+    # called when the user makes a GET request (i.e. when they
+    # visit the website in their browser).
+    # The "methods" argument is a list of all the HTTP methods
+    # that this function should respond to. In this case, we
+    # only want to respond to GET requests, so we only include
+    # "GET" in the list.
+    return render_template('index.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def get_data():
+    """
+    This function is called when a user visits the website
+    (i.e. when they make a GET request to the root URL).
+    It is also called when the user submits a form on the website
+    (i.e. when they make a POST request to the root URL).
+    """
+    # Check if the request is a POST request
+    if request.method == 'POST':
+        """
+        If the request is a POST request, then we know that the user
+        has submitted a form on the website. We can access the data
+        from the form using the request.form dictionary.
+        """
+        # Get the values from the form
+        age = request.form['age']
+        gender = request.form['gender']
+        Polyuria = request.form['Polyuria']
+        Polydipsia = request.form['Polydipsia']
+        Weight = request.form['Weight']
+        Weakness = request.form['Weakness']
+        Polyphagia = request.form['Polyphagia']
+        Thrush = request.form['Thrush']
+        Blurring = request.form['Blurring']
+        Itching = request.form['Itching']
+        Irritability = request.form['Irritability']
+        Healing = request.form['Healing']
+        Paresis = request.form['Paresis']
+        Stiffness = request.form['Stiffness']
+        Alopecia = request.form['Alopecia']
+        Obesity = request.form['Obesity']
+        
+        # Create a new list with the values from the form
+        newpat = [[age,gender,Polyuria,Polydipsia,Weight,Weakness,Polyphagia,Thrush,Blurring,Itching,Irritability,Healing,Paresis,Stiffness,Alopecia,Obesity]]
+        
+        # Use the model to make a prediction based on the values
+        result = clf.predict(newpat)
+        
+        # If the prediction is 1, then the user has diabetes
+        if result == 1:
+            val = "Diabetes"
+        # Otherwise, the user does not have diabetes
+        else:
+            val = "No Diabetes"
+            
+    # Render the HTML template with the result
+    return render_template('/templates/index.html',value=val)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
